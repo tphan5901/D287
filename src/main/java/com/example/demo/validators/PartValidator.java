2019b0fb980c81a2309e4be617e94748fe6cd11b
Your Name <tphan5901@gmail.com>
2024-10-14 18:37:17 -0700

    update

--- src/main/java/com/example/demo/validators/PartValidator.java
+++ src/main/java/com/example/demo/validators/PartValidator.java
@@ -8,17 +8,15 @@ public class PartValidator implements ConstraintValidator<ValidPart, Part> {
     @Override
     public boolean isValid(Part part, ConstraintValidatorContext context) {
         boolean isValid = true;
-        // Validate min is not greater than max
         if (part.getMinimum() > part.getMaximum()) {
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("Minimum cannot be greater than Maximum.")
+            context.buildConstraintViolationWithTemplate("Min cannot be greater than Max.")
                     .addPropertyNode("minimum").addConstraintViolation();
             isValid = false;
         }
-        // Validate inv is between min and max
         if (part.getInv() < part.getMinimum() || part.getInv() > part.getMaximum()) {
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("Inventory must be between Minimum and Maximum.")
+            context.buildConstraintViolationWithTemplate("Inventory must be between Min and Max.")
                     .addPropertyNode("inv").addConstraintViolation();
             isValid = false;
         }
